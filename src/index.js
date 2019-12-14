79822e2fa82e435b7451508f037319a224445108
Grady Simon <grady@gradysimon.com>
2019-12-13 22:25:03 -0800

    Basic spinning cube in Three.js. Config problems.
    Typescript throws type errors related to OffscreenCanvas, and so tsc
    won't compile the code. Parcel has no problem with it however, so
    `parcel src/index.html` works fine and Three.js renders its scene
    sucessfully.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+console.log('hello, world!');
+var init = function () {
+    return 'cat';
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-const myFunc: void = () => { console.log("hello, world"); };
+import * as THREE from 'three';
 
-myFunc();
+console.log('hello, world!');
+
+const init = () => {
+    let scene = new THREE.Scene();
+
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    let camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    camera.position.z = 5;
+
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    let cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const animate = () {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    }
+    animate();
+};
+
+init();
