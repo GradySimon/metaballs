a2387c65bc738180b76a4a47a7f5ec5cd9fceb2c
Grady Simon <grady@gradysimon.com>
2019-12-15 00:18:20 -0800

    Two interacting metaballs
    Not happy with how the coordinates work. Finding it difficult to get
    balls in right spot on screen.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,19 @@ interface ShaderState {
   mesh: THREE.Mesh;
 }
 
+interface Metaball {
+  position: [number, number];
+  radius: number;
+}
+
 interface AnimationState {
   scene: THREE.Scene;
   camera: THREE.Camera;
   renderer: THREE.Renderer;
   shader: ShaderState;
-  objs?: Record<string, any>;
+  aspectRatio: number;
+  metaballs?: Metaball[];
+  time: { start: number, elapsed: number };
 }
 
 const fetchText = async (url: string): Promise<string> => {
@@ -30,15 +37,15 @@ const initShader = async () => {
         window.innerHeight)
     },
     u_mouse: { type: "v2", value: new THREE.Vector2() },
-    u_metaball_pos: { type: "v2v", value: [0.0, 0.0, 0.0, 0.0] },
+    u_metaball_pos: { type: "v2v", value: [] },
     u_metaball_radius: {
-      type: "fv", value: [0.2, 0.2]
+      type: "fv", value: []
     },
     u_num_metaballs: {
       type: "int", value: 2
     },
     u_threshold: {
-      type: "float", value: 0.5
+      type: "float", value: 0.3
     },
   };
   let material = new THREE.ShaderMaterial({
@@ -69,14 +76,81 @@ const init = async (): Promise<AnimationState> => {
     scene,
     camera,
     renderer,
-    shader: { uniforms, mesh }
+    aspectRatio,
+    shader: { uniforms, mesh },
+    time: { start: Date.now(), elapsed: 0 }
   };
 };
 
-const update = (state: AnimationState): AnimationState => {
-  return state;
+interface Oscillation {
+  inScale: number;
+  inOffset: number;
+  outMin: number;
+  outMax: number;
+  fn: (x: number) => number;
 }
 
+const oscillate = (t: number, {
+  inScale = 1 / 1000,
+  inOffset = 0,
+  outMin = 0,
+  outMax = 1,
+  fn = Math.sin,
+}: Oscillation): number => {
+  const range = outMax - outMin;
+  return fn(t * inScale + inOffset) * range / 2 + (range / 2) + outMin;
+};
+
+const metaballState = (state: AnimationState): Metaball[] => {
+  console.log([oscillate(state.time.elapsed, {}),
+  oscillate(state.time.elapsed, { fn: Math.cos })])
+  let metaballs: Metaball[] = [
+    {
+      position: [
+        oscillate(state.time.elapsed,
+          {
+            outMin: 0.1,
+            outMax: 1 / state.aspectRatio
+          }),
+        oscillate(state.time.elapsed,
+          {
+            outMin: 0.1,
+            outMax: 1 / state.aspectRatio, fn: Math.cos
+          })],
+      radius: 0.02;
+    }, { position: [0.2, 0.2], radius: 0.02 },
+  ];
+  return metaballs;
+}
+
+interface MetaballUniformValues {
+  u_metaball_pos: number[];
+  u_metaball_radius: number[];
+}
+
+const metaballsToUniforms = (metaballs: Metaball[]): MetaballUniformValues => {
+  let flat_positions: number[] = [];
+  let radii: number[] = [];
+  for (const metaball of metaballs) {
+    flat_positions.push(metaball.position[0], metaball.position[1]);
+    radii.push(metaball.radius);
+  }
+  return {
+    u_metaball_pos: flat_positions,
+    u_metaball_radius: radii,
+  }
+}
+
+const update = (state: AnimationState): AnimationState => {
+  state.time.elapsed = Date.now() - state.time.start;
+
+  const metaballs = metaballState(state);
+  const { u_metaball_pos, u_metaball_radius } = metaballsToUniforms(metaballs);
+  state.shader.uniforms['u_metaball_pos'].value = u_metaball_pos;
+  state.shader.uniforms['u_metaball_radius'].value = u_metaball_radius;
+
+  return state;
+}
 
 const animate = (state: AnimationState) => {
   state = update(state);
--- static/shader/metaball.frag
+++ static/shader/metaball.frag
@@ -3,8 +3,9 @@ precision mediump float;
 #endif
 
 #define MAX_NUM_METABALLS 2
-#define THRESHOLD 0.424
 #define RADIUS 0.065
+
+#define DEBUG_DENSITY false
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
@@ -20,6 +21,13 @@ vec4 grayscale(float value) {
 }
 
 vec4 color_for_density(float density) {
+    if (DEBUG_DENSITY) {
+      if ((u_threshold - 0.005 < density)
+           && (density < u_threshold + 0.005)) {
+        return vec4(1., 0., 0., 1.);
+      }
+      return grayscale(density);
+    }
     if (density > u_threshold) {
         return grayscale(0.0);
     }
@@ -27,14 +35,17 @@ vec4 color_for_density(float density) {
 }
 
 void main() {
+    // vec2 frag_pos = gl_FragCoord.xy / vec2(u_resolution[0], u_resolution[0]);
+    float aspect_ratio = u_resolution.x / u_resolution.y;
     vec2 frag_pos = gl_FragCoord.xy / u_resolution;
+    frag_pos.y /= aspect_ratio;
 
     float density = 0.;
     for (int i = 0; i < MAX_NUM_METABALLS; i++) {
         if (i >= u_num_metaballs) {
           continue;
         }
-        vec2 pos = u_metaball_pos[i] / u_resolution;
+        vec2 pos = u_metaball_pos[i];
         float single_ball_density =
           (u_metaball_radius[i] * u_metaball_radius[i])
           / dot((frag_pos - pos), (frag_pos - pos));
